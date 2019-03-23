770744158fe5ada4d1bf639087dbc289a10143fc
Ben Sykes <bsykes888@gmail.com>
2019-03-23 16:23:30 -0400

    Updated.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const path = require('path')
 const logger = require('./logger.js')
 const hhmmss = require('./hhmmss.js')
+const opn = require('opn')
 
 // Initialize the application.
 logger.info('Initializing application server...')
@@ -27,18 +28,38 @@ process.stdin.setEncoding('utf8')
 process.stdin.on('data', function (text) {
   var command = text.trim()
   if (command[0] === '/') {
+    
+    // Command: /help
     if (command === '/help') {
-      logger.data('COMMANDS: help quit config uptime')
+      logger.data('COMMANDS: help open quit config uptime')
+    
+    // Command: /open
+    } else if (command === '/open') {
+      try {
+        opn('http://localhost:' + PORT.toString(), {app: 'chromium'})
+      } catch {
+        opn('http://localhost:' + PORT.toString())
+      }
+      
+    // Command: /quit
     } else if (command === '/quit') {
       logger.warn('Quit requested, exiting now...')
       process.exit()
+    
+    // Command: /config
     } else if (command === '/config') {
       logger.data('CONFIG: Port='+PORT.toString())
+      
+    // Command: /uptime
     } else if (command === '/uptime') {
       logger.data('UPTIME: '+hhmmss.toHHMMSS(process.uptime()+""))
+      
+    // Invalid command.
     } else {
       logger.error('Invalid command.')
     }
+    
+  // No command.
   } else {
     logger.data('ECHO: '+command)
   }
