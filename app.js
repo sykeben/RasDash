c05831f7612401c87270df8681b1efd0370f4dc9
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2019-03-26 10:29:48 -0400

    Added multiple dashboard pages.

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.set('view engine', 'ejs')
 logger.info('Configuring application requests...')
 app.get('/', (req, res) => res.redirect('/dash/0'))
 app.get('/dash/0', (req, res) => res.render('pages/dash_0'))
+app.get('/dash/1', (req, res) => res.render('pages/dash_1'))
 app.get('/about', (req, res) => res.render('pages/about'))
 
 // Start server.
