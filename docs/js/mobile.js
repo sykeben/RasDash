514ad7297a6fc14e0e770af3e2398e08a210ed13
Benjamin Sykes <33205078+sykeben@users.noreply.github.com>
2019-03-28 14:21:35 -0400

    Removed extranious stuff not related to demo.

--- docs/js/mobile.js
+++
@@ -1,8 +0,0 @@
-const minWidth = 815;
-const screenWidth = window.innerWidth;
-const errorModal = $('#sizeError');
-
-// If the screen is really small, show an error.
-window.onload = function() {
-    if (screenWidth < minWidth) errorModal.modal('show');
-};
